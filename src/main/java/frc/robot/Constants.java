0ddd0c732caf0fb3c5eddb0db4a1d57dbe72711c
Kyle Huang <kylehuang06@gmail.com>
2023-09-02 12:12:37 -0700

    Add Wrist
    Co-Authored-By: Kylie <123788168+kymc-6@users.noreply.github.com>
    Co-Authored-By: silverx257 <109992135+silverx257@users.noreply.github.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -32,7 +32,30 @@ public final class Constants {
     public static final double kDriveOneMinusAlpha = 0.88235;
     public static final double kErrorBound = 0;
   }
+  public static class WristConstants {
+    public static final int kLeftWristID = 0;
+    public static final int kRightWristID = 0;
+    public static final int kLeftEncoderID = 0;
+    public static final int kWristStow = 0;
+    
+    public static final int kWristLowerLimit = 0;
+    public static final int kWristUpperLimit = 0;
 
+    public static final double kWristP = 0;
+    public static final double kWristI = 0;
+    public static final double kWristD = 0;
+    public static final double kWristF = 0;
+    public static final double kLowPassAlpha = 0.11765;
+    public static final double kJoystickScale = 500;
+    public static final int kWristCruiseVelocity = 0;
+    public static final int kWristMotionAcceleration = 0;
+    public static final double kWristDeadband = 0.05;
+    public static final double kWristFF = 0;
+    public static final double kWristGearRatio = 27;
+    public static final double kFalconTicksPerAbsoluteTicks = kWristGearRatio * 2048/1024;
+    public static final double kDegreesPerTick = 360 / kWristGearRatio / 2048;
+
+  }
   public static class ControllerConstants {
     public static final double kDeadband = 0.05;
     public static final double kRotationDeadband = 0.1;

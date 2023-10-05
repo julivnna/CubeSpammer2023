7d093553a596e3a46a71471d9c59cc9a67ea792c
Kaden Bautista <k07bautista@gmail.com>
2023-10-04 22:55:34 -0700

    planned out auto

--- src/main/java/frc/robot/commands/autos/EasyTesting.java
+++ src/main/java/frc/robot/commands/autos/EasyTesting.java
@@ -16,22 +16,73 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import frc.robot.Constants.PathPlannerConstants;
+import frc.robot.Constants.ShooterConstants;
 import frc.robot.Constants.SwerveDriveConstants;
+import frc.robot.Constants.WristConstants;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Wrist;
 import frc.robot.subsystems.swerve.SwerveDrivetrain;
 
 public class EasyTesting extends SequentialCommandGroup {
+    private static List<Trajectory> trajectories = new ArrayList<Trajectory>();
     private static List<SwerveControllerCommand> trajectoryCommands = new ArrayList<SwerveControllerCommand>();
 
     public EasyTesting(boolean testing, SwerveDrivetrain swerve, Shooter shooter, Wrist wrist) {
-        Trajectory initToCube1, cube1ToScore, scoreToCube2, cube2ToScore2, score2ToOutside;
-
         addTrajectory(3, swerve);
         
         addCommands(
             Commands.sequence(
-                trajectoryCommands.get(0)
+                Commands.runOnce(() -> swerve.getImu().zeroAll()),
+                Commands.runOnce(() -> swerve.resetOdometry(trajectories.get(0).getInitialPose())),
+
+                //preload
+                wrist.motionMagicCommand(WristConstants.kWristStow),
+                shooter.outtakeHigh(),
+
+                //go to first cube and intake
+                Commands.parallel(
+                    wrist.motionMagicCommand(WristConstants.kWristLowPickup),
+                    trajectoryCommands.get(0),
+                    shooter.setPower(ShooterConstants.kTopIntakePower.get(), ShooterConstants.kBottomIntakePower.get())
+                ),
+
+                Commands.runOnce(() -> swerve.towModules()),
+
+                //go to score 2nd piece
+                Commands.waitSeconds(0.5),
+                wrist.motionMagicCommand(WristConstants.kWristStow),
+                trajectoryCommands.get(1),
+                shooter.outtakeLow(),
+
+                Commands.runOnce(() -> swerve.towModules()),
+
+                //go to second cube and intake
+                Commands.parallel(
+                    trajectoryCommands.get(2),
+                    Commands.sequence(
+                        Commands.waitSeconds(0.5),
+                        wrist.motionMagicCommand(WristConstants.kWristLowPickup)
+                    ),
+                    shooter.setPower(ShooterConstants.kTopIntakePower.get(), ShooterConstants.kBottomIntakePower.get())
+                ),
+
+                Commands.runOnce(() -> swerve.towModules()),
+
+                //got to score 3rd piece
+                Commands.waitSeconds(0.5),
+                Commands.parallel(
+                    wrist.motionMagicCommand(WristConstants.kWristStow),
+                    trajectoryCommands.get(3)
+                ),
+                shooter.outtakeHigh(),
+
+                Commands.runOnce(() -> swerve.towModules()),
+
+                //leave community
+                Commands.waitSeconds(0.5),
+                trajectoryCommands.get(4),
+
+                Commands.runOnce(() -> swerve.towModules())
             )
         );
     }
@@ -56,6 +107,7 @@ public class EasyTesting extends SequentialCommandGroup {
         }
         
         Trajectory temp = TrajectoryGenerator.generateTrajectory(pose2ds, config);
+        trajectories.add(temp);
         trajectoryCommands.add(new SwerveControllerCommand(temp, swerve::getPose, SwerveDriveConstants.kDriveKinematics, translation, translation, rotation, swerve::setModuleStates, swerve));
     }
 }
